ec0016041a6bc3e6c7e0ed5d384a956137883f17
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-04 14:43:02 -0700

    added game urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('games/', views.games_index, name='index'),
   path('games/<int:game_id>/', views.games_detail, name='detail'),
+  path('games/create/', views.GameCreate.as_view(), name='games_create'),
+  path('games/<int:pk>/update', views.GameUpdate.as_view(), name='games_update'),
+  path('games/<int:pk>/delete', views.GameDelete.as_view(), name='games_delete'),
 ]
