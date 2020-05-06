bf6f8d0ef84033a0ad0aaad252fb0a6316976a2d
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-05-05 17:01:13 -0700

    merged

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
   path('games/create/', views.GameCreate.as_view(), name='games_create'),
   path('games/<int:pk>/update', views.GameUpdate.as_view(), name='games_update'),
   path('games/<int:pk>/delete', views.GameDelete.as_view(), name='games_delete'),
-  path('games/<int:pk>/add_review/', views.add_review, name='add_review'),
+  path('games/<int:pk>', views.add_review, name='add_review'),
   # path('games/<int:pk>/update_review/', views.update_review, name='update_review'),
   # path('games/<int:pk>/delete_review/', views.delete_review, name='delete_review'),
 
